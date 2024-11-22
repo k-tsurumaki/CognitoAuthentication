0cd813af987fcd6fb44e4b7f069a2ec397dea99c
k-tsurumaki <kazuki67266726@gmail.com>
2024-11-23 08:21:39 +0900

    Initial commit

---
+++ src/main/java/com/example/CognitoAuthentication/CognitoAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.CognitoAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoAuthenticationApplication.class, args);
+	}
+
+}
